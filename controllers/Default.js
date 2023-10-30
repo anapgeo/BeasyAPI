d3a8e4c2e9b5fa92d7a49182bfa237fcefbfb1b8
anapgeo <anastasiadis.george1512@gmail.com>
2023-10-30 16:27:36 +0200

    Initial commit

---
+++ controllers/Default.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Default = require('../service/DefaultService');
+
+module.exports.appointmentAppointmentIdReturnPOST = function appointmentAppointmentIdReturnPOST (req, res, next, body, appointmentId) {
+  Default.appointmentAppointmentIdReturnPOST(body, appointmentId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.appointmentsAppointmentIdAppointmentPOST = function appointmentsAppointmentIdAppointmentPOST (req, res, next, body, appointmentId) {
+  Default.appointmentsAppointmentIdAppointmentPOST(body, appointmentId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.professionalsGET = function professionalsGET (req, res, next) {
+  Default.professionalsGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.professionalsPOST = function professionalsPOST (req, res, next, body) {
+  Default.professionalsPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.professionalsProfessionalIDDELETE = function professionalsProfessionalIDDELETE (req, res, next, professionalID) {
+  Default.professionalsProfessionalIDDELETE(professionalID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.professionalsProfessionalIDGET = function professionalsProfessionalIDGET (req, res, next, professionalID) {
+  Default.professionalsProfessionalIDGET(professionalID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersGET = function usersGET (req, res, next) {
+  Default.usersGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersPOST = function usersPOST (req, res, next, body) {
+  Default.usersPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersUserIdAppointmentsGET = function usersUserIdAppointmentsGET (req, res, next, userId) {
+  Default.usersUserIdAppointmentsGET(userId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersUserIdDELETE = function usersUserIdDELETE (req, res, next, userId) {
+  Default.usersUserIdDELETE(userId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersUserIdGET = function usersUserIdGET (req, res, next, userId) {
+  Default.usersUserIdGET(userId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.usersUserIdPUT = function usersUserIdPUT (req, res, next, body, userId) {
+  Default.usersUserIdPUT(body, userId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ service/DefaultService.js
@@ -0,0 +1,236 @@
+'use strict';
+
+
+/**
+ * Return a appointment
+ *
+ * body ReturnRequest 
+ * appointmentId Integer 
+ * no response value expected for this operation
+ **/
+exports.appointmentAppointmentIdReturnPOST = function(body,appointmentId) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Arrange an appointment
+ *
+ * body AppointmentRequest 
+ * appointmentId Integer 
+ * no response value expected for this operation
+ **/
+exports.appointmentsAppointmentIdAppointmentPOST = function(body,appointmentId) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get a list of professionals
+ *
+ * returns List
+ **/
+exports.professionalsGET = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "profession" : "profession",
+  "name" : "name",
+  "id" : 0,
+  "services" : [ {
+    "availableservice" : "availableservice"
+  }, {
+    "availableservice" : "availableservice"
+  } ]
+}, {
+  "profession" : "profession",
+  "name" : "name",
+  "id" : 0,
+  "services" : [ {
+    "availableservice" : "availableservice"
+  }, {
+    "availableservice" : "availableservice"
+  } ]
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Add a new professional
+ *
+ * body Professionals 
+ * no response value expected for this operation
+ **/
+exports.professionalsPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Delete a professional
+ *
+ * professionalID Integer 
+ * no response value expected for this operation
+ **/
+exports.professionalsProfessionalIDDELETE = function(professionalID) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get details of a professionals
+ *
+ * professionalID Integer 
+ * returns Professionals
+ **/
+exports.professionalsProfessionalIDGET = function(professionalID) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "profession" : "profession",
+  "name" : "name",
+  "id" : 0,
+  "services" : [ {
+    "availableservice" : "availableservice"
+  }, {
+    "availableservice" : "availableservice"
+  } ]
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Get a list of users
+ *
+ * returns List
+ **/
+exports.usersGET = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "name" : "name",
+  "id" : 0,
+  "email" : "email"
+}, {
+  "name" : "name",
+  "id" : 0,
+  "email" : "email"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Create a new user
+ *
+ * body User 
+ * no response value expected for this operation
+ **/
+exports.usersPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get a list of appointments by a user
+ *
+ * userId Integer 
+ * returns List
+ **/
+exports.usersUserIdAppointmentsGET = function(userId) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "professionalId" : 6,
+  "userId" : 0,
+  "appointmentDate" : "2000-01-23T04:56:07.000+00:00"
+}, {
+  "professionalId" : 6,
+  "userId" : 0,
+  "appointmentDate" : "2000-01-23T04:56:07.000+00:00"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Delete a user
+ *
+ * userId Integer 
+ * no response value expected for this operation
+ **/
+exports.usersUserIdDELETE = function(userId) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get details of a user
+ *
+ * userId Integer 
+ * returns User
+ **/
+exports.usersUserIdGET = function(userId) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "name" : "name",
+  "id" : 0,
+  "email" : "email"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Update user details
+ *
+ * body UserUpdate 
+ * userId Integer 
+ * no response value expected for this operation
+ **/
+exports.usersUserIdPUT = function(body,userId) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
