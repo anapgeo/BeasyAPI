1235b1bc3f4a3bb0161d683e11f3caa2379d4187
anapgeo <anastasiadis.george1512@gmail.com>
2024-01-11 22:27:48 +0200

    Remove complexity from writer.js

--- utils/writer.js
+++ utils/writer.js
@@ -11,42 +11,39 @@ exports.respondWithCode = function(code, payload) {
 
 // Function to write JSON response to the provided HTTP response object
 var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+  var { code, payload } = determineCodeAndPayload(arg1, arg2);
+  code = code || 200;
 
-  // Check if arg1 is an instance of ResponsePayload, if true, recursively call writeJson
-  if(arg1 && arg1 instanceof ResponsePayload) {
-    writeJson(response, arg1.payload, arg1.code);
-    return;
-  }
-
-  // Determine code and payload from provided arguments
-  if(arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-  if(code && arg1) {
-    payload = arg1;
-  }
-  else if(arg1) {
-    payload = arg1;
-  }
-
-  // If no code is provided, default to 200
-  if(!code) {
-    code = 200;
-  }
-
-  // If payload is an object, convert it to a JSON string with indentation
-  if(typeof payload === 'object') {
+  if (typeof payload === 'object') {
     payload = JSON.stringify(payload, null, 2);
   }
 
-  // Set response headers and end the response
-  response.writeHead(code, {'Content-Type': 'application/json'});
+  setResponseHeadersAndEnd(response, code, payload);
+}
+
+// Helper function to determine code and payload from arguments
+function determineCodeAndPayload(arg1, arg2) {
+  var code, payload;
+
+if (arg1 && arg1 instanceof ResponsePayload) {
+    return determineCodeAndPayload(arg1.payload, arg1.code);
+  }
+
+  if (arg2 && Number.isInteger(arg2)) {
+    code = arg2;
+  } else if (arg1 && Number.isInteger(arg1)) {
+    code = arg1;
+  }
+
+  if (arg1) {
+    payload = arg1;
+  }
+
+  return { code, payload };
+}
+
+// Helper function to set response headers and end the response
+function setResponseHeadersAndEnd(response, code, payload) {
+  response.writeHead(code, { 'Content-Type': 'application/json' });
   response.end(payload);
 }
