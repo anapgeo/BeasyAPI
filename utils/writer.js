f536ddebd58f1f48fb4394fc6aadc62bbf0c8d7f
anapgeo <anastasiadis.george1512@gmail.com>
2024-01-11 22:33:30 +0200

    Fix writer.js

--- utils/writer.js
+++ utils/writer.js
@@ -10,29 +10,3 @@ exports.respondWithCode = function(code, payload) {
 }
 
 
-// Helper function to determine code and payload from arguments
-function determineCodeAndPayload(arg1, arg2) {
-  var code, payload;
-
-if (arg1 && arg1 instanceof ResponsePayload) {
-    return determineCodeAndPayload(arg1.payload, arg1.code);
-  }
-
-  if (arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  } else if (arg1 && Number.isInteger(arg1)) {
-    code = arg1;
-  }
-
-  if (arg1) {
-    payload = arg1;
-  }
-
-  return { code, payload };
-}
-
-// Helper function to set response headers and end the response
-function setResponseHeadersAndEnd(response, code, payload) {
-  response.writeHead(code, { 'Content-Type': 'application/json' });
-  response.end(payload);
-}
